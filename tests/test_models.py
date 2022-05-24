d1a0dfafa712e7b69077f1ca8e1f7bed77d95aab
Johnathon Hyland <johnathon.hyland@ligo.org>
2022-05-24 13:50:01 +0100

    Add parameterisation mean, min, max test cases

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,56 +5,40 @@ import numpy.testing as npt
 import pytest
 
 
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0], [0, 0], [0, 0]], [0, 0]),
+        ([[1, 2], [3, 4], [5, 6]], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
     from inflammation.models import daily_mean
-
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
 
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-    from inflammation.models import daily_mean
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
-
-
-def test_daily_max_float():
-    """Test that max function works for an array of floats."""
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0.0, 0.2], [0.2, 0.1], [0.3, 0.4]], [0.3, 0.4]),
+        ([[1, 2], [3, 4], [5, 6]], [5, 6]),
+    ])
+def test_daily_max(test, expected):
+    """Test min function works for array of floats and positive integers."""
     from inflammation.models import daily_max
-
-    test_input = np.array([[0.1, 0.1],
-                           [0.2, 0.2],
-                           [0.3, 0.4]])
-    test_result = np.array([0.3, 0.4])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
 
-def test_daily_min_float():
-    """Test that min function works for an array of floats."""
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0.0, 0.2], [0.2, 0.1], [0.3, 0.4]], [0.0, 0.1]),
+        ([[1, 2], [3, 4], [5, 6]], [1, 2]),
+    ])
+def test_daily_min(test, expected):
+    """Test min function works for array of floats and positive integers."""
     from inflammation.models import daily_min
-
-    test_input = np.array([[0.0, 0.2],
-                           [0.2, 0.1],
-                           [0.3, 0.4]])
-    test_result = np.array([0.0, 0.1])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_min(test_input), test_result)
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
 
 def test_daily_min_string():
